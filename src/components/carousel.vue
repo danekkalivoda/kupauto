36457d16969cf196f85bba370f83a23f9389befe
Daniel Kalivoda <danek.kalivoda@gmail.com>
2019-08-25 21:46:49 +0200

    styling

--- src/components/carousel.vue
+++ src/components/carousel.vue
@@ -1,20 +1,38 @@
 <template>
 
-    <vueper-slides class="no-shadow" :visible-slides="3" :slide-ratio="1/4" :dragging-distance="70">
-      <vueper-slide v-for="(slide, i) in slides" :key="i">
-        <div slot="slideContent">
-          <v-layout>
-          <div>
-            <div>{{ slide.title }}</div>
-            <div>{{ slide.content }}</div>
-            <div class="block relative aspect-ratio-4/3 transition group-hover:scale-110">
-              <img class="w-full h-full absolute top-0 left-0 object-cover z-10" :src="slide.image" :alt="slide.title">
-            </div>
+  <vueper-slides
+    :visible-slides="5"
+    :dragging-distance="70"
+    :bullets="false"
+    slide-multiple
+    :arrows-outside="false"
+    :slide-ratio="1/5"
+    :breakpoints="{
+      1200: {
+        visibleSlides: 3,
+        slideRatio: 1/3
+      },
+      640: {
+        visibleSlides: 2,
+        slideRatio: 1/2
+      },
+      640: {
+        visibleSlides: 1,
+        slideRatio: 1/1
+      }
+    }"
+    class="no-shadow">
+    <vueper-slide v-for="(slide, i) in slides" :key="i" >
+      <div slot="slideContent">
+        <v-layout>
+          <div class="w-full">
+            <div class="block relative aspect-ratio-4/3 overflow-hidden transition group-hover:scale-110 bg-cover" :style="{ backgroundImage: 'url(' + slide.image + ')' }"></div>
+            <div class="text-green-500 bg-gray-800 p-4">{{ slide.title }}</div>
           </div>
-          </v-layout>
-        </div>
-      </vueper-slide>
-    </vueper-slides>
+        </v-layout>
+      </div>
+    </vueper-slide>
+  </vueper-slides>
 
 </template>
 
